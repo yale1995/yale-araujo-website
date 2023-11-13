3ded45e1eaf851af4c70bd6f252448d34b64dd49
Yale Araujo <yale850@gmail.com>
2023-11-13 17:30:57 -0300

    refactor: adding time out to request

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,9 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      httpOptions: {
+        timeout: 40000,
+      },
       profile(profile: GoogleProfile) {
         return {
           id: profile.sub,
