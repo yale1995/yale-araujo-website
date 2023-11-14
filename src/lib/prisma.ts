9b3aa712be22f10d2c89ccde86c7c6251f4015d8
Yale Araujo <yale850@gmail.com>
2023-11-14 11:01:05 -0300

    refactor: prisma logs

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = global.prismadb || new PrismaClient()
+export const prisma =
+  global.prismadb ||
+  new PrismaClient({
+    log: ['query'],
+  })
 
 if (process.env.NODE_ENV === 'production') global.prismadb = prisma
