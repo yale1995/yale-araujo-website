310907a379672822776ef6801e4c7e47aa0f3d94
Yale Araujo <yale850@gmail.com>
2023-11-17 10:49:39 -0300

    feat:

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,13 +3,18 @@ import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
 
 export const authOptions: NextAuthOptions = {
-  debug: true,
   adapter: PrismaAdapter(),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      // the profile function works to rename all data provided by google
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
       profile(profile: GoogleProfile) {
         return {
           id: profile.sub,
@@ -22,13 +27,16 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    // by default on next auth session only is returned {name, email}, but now is going being returned all data user: {name, email, avatar_url}
     async session({ session, user }) {
       return {
         ...session,
         user,
       }
     },
+  },
+  debug: true,
+  session: {
+    maxAge: 60 * 60 * 30,
   },
   secret: process.env.NEXTAUTH_SECRET,
 }
