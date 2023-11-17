a5e4616107a7b1ff3934b6bb9871fe1214fcfe2d
Yale Araujo <yale850@gmail.com>
2023-11-17 11:18:38 -0300

    refactor: authentication improviment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.ctfassets.net',
-      },
-    ],
+    domains: ['images.ctfassets.net'],
   },
 }
 
--- src/app/(site)/components/ButtonAuthUser/index.tsx
+++ src/app/(site)/components/ButtonAuthUser/index.tsx
@@ -2,29 +2,36 @@
 
 import { EnvelopeSimpleOpen, Spinner } from '@/app/components/Icon'
 import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 import { useCallback } from 'react'
 
 export function ButtonAuthUser() {
   const { status: sessionStatus } = useSession()
+  const router = useRouter()
 
   const handleSignInUser = useCallback(async () => {
-    await signIn('google')
-  }, [])
+    if (sessionStatus === 'unauthenticated') {
+      await signIn('google', { redirect: false, callbackUrl: '/publications' })
+    }
+
+    router.push('/publications')
+  }, [router, sessionStatus])
 
   const isLoading = sessionStatus === 'loading'
 
   return (
     <button
       type="button"
+      disabled={isLoading}
       onClick={handleSignInUser}
-      className="flex gap-2 w-full items-center justify-center bg-gray-200 hover:bg-gray-50 duration-500 ring-1 ring-gray-900 py-2 px-4 rounded-lg text-gray-900 font-semibold text-lg"
+      className="flex gap-2 w-full items-center justify-center bg-gray-200 enabled:hover:bg-gray-50 disabled:bg-gray-200/40 duration-500 ring-1 ring-gray-900 py-2 px-4 rounded-lg text-gray-900 font-semibold text-lg"
     >
       {isLoading ? (
         <Spinner className="text-gray-900 text-2xl animate-spin" />
       ) : (
         <EnvelopeSimpleOpen className="text-gray-900 text-2xl" />
       )}
-      {sessionStatus === 'authenticated' ? 'Authenticated' : 'Publications'}
+      Publications
     </button>
   )
 }
